efb18207e9e534b19bf8cd0db193c219a6e14bda
Ahmed Abo-zaid <ahmedabozaid493@gmail.com>
2021-06-07 14:55:38 +0200

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-
-
-            if(event.type == pygame.MOUSEBUTTONDOWN and isPlayerTurn):
-                mouseX = event.pos[0] 
-                mouseY = event.pos[1]
-                #add player input
-                idx = mapCords.getIdx(mouseX,mouseY)
